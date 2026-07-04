e348565609e30fc8c4b64e48f78d3378b09eedd4
abdelmajidelayachi <abde.elayachi01@gmail.com>
2026-07-04 11:59:37 +0100

    add folder of the web techno tp

---
+++ technologie-web/tp-crud-php/etape1/view.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../config.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Détails de l'utilisateur</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+<body>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
